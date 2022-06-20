85917974bd10f8c23e214d1c0f8117505895bc8a
Bhaumik Chadgal <69686719+BHAUMIK3127@users.noreply.github.com>
2022-06-21 00:12:07 +0530

    Create unwinnable_RPSgame.py

---
+++ unwinnable_RPSgame.py
@@ -0,0 +1,29 @@
+
+rock = "rock"
+paper = "paper"
+scissors="scissors"
+
+print("ROCK , PAPER OR SCISSORS?")
+
+a=input()
+
+if a =="rock" :
+    print("PAPER HAHA I WIN")
+
+if a =="paper" :
+    print("SCISSORS HAHA I WIN")
+
+if a =="scissors" :
+    print("ROCK HAHA I WIN")
+
+print("still wanna continue?")
+
+print("Y FOR YES , N FOR NO")
+a1=input()
+a1.capitalize()
+
+if a1=="Y":
+    print("U aint tired of losing but im tired of winning so imma leave")
+
+if a1=="N":
+    print("ALRIGHT C ya later")
